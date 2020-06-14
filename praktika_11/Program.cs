d835159008fc16b7614ef1e63cbe80354a7a6e64
ChDarina <idkkunmaybechan@gmail.com>
2020-06-14 19:17:07 +0500

    были удалены ненужные переменные

--- praktika_11/Program.cs
+++ praktika_11/Program.cs
@@ -6,15 +6,14 @@ namespace praktika_11
     {
         static void Main(string[] args)
         {
-            int size = 11, x=5, y=5;
+            int size = 11, x = 5, y = 5;
             Random a = new Random();
-            char[] bukvy = new char[26] {'A', 'B', 'C', 'D', 'E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+            char[] bukvy = new char[26] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
             string original = "", crypted = "", decrypted = "";
             char manual;
             char[,] matrix = new char[size, size];
             char[,] decryptedmatrix = new char[size, size];
             char[] cryptedarr = new char[121];
-
             for (int i = 0; i < size; i++)
             {
                 for (int j = 0; j < size; j++)
@@ -26,16 +25,16 @@ namespace praktika_11
             }
             crypted += matrix[x, y];
             Console.WriteLine("Оригинальная строка:\n" + original);
-            for (int i = 0, x1=2, y1=2; i < 5; i++, x1+=2, y1+=2)
+            for (int i = 0, xy = 2; i < 5; i++, xy += 2)
             {
                 crypted += matrix[x, --y];
-                for (int j=1; j < x1 ; j++)
+                for (int j = 1; j < xy; j++)
                     crypted += matrix[++x, y];
-                for (int j = 1; j <= y1; j++)
+                for (int j = 1; j <= xy; j++)
                     crypted += matrix[x, ++y];
-                for (int j = 1; j <= x1; j++)
+                for (int j = 1; j <= xy; j++)
                     crypted += matrix[--x, y];
-                for (int j = 1; j <= y1; j++)
+                for (int j = 1; j <= xy; j++)
                     crypted += matrix[x, --y];
             }
             cryptedarr = crypted.ToCharArray();
@@ -43,17 +42,17 @@ namespace praktika_11
             x = 5;
             y = 5;
             decryptedmatrix[x, y] = cryptedarr[0];
-            for (int i = 0, z = 1, x1 = 2, y1 = 2; i < 5; i++, x1 += 2, y1 += 2)
+            for (int i = 0, z = 1, xy = 2; i < 5; i++, xy += 2)
             {
                 decryptedmatrix[x, --y] = cryptedarr[z];
                 z++;
-                for (int j = 1; j < x1; j++, z++)
+                for (int j = 1; j < xy; j++, z++)
                     decryptedmatrix[++x, y] = cryptedarr[z];
-                for (int j = 1; j <= y1; j++, z++)
+                for (int j = 1; j <= xy; j++, z++)
                     decryptedmatrix[x, ++y] = cryptedarr[z];
-                for (int j = 1; j <= x1; j++, z++)
+                for (int j = 1; j <= xy; j++, z++)
                     decryptedmatrix[--x, y] = cryptedarr[z];
-                for (int j = 1; j <= y1; j++, z++)
+                for (int j = 1; j <= xy; j++, z++)
                     decryptedmatrix[x, --y] = cryptedarr[z];
             }
             for (int i = 0; i < size; i++)
